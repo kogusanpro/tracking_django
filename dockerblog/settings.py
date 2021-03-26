b15d5f9a027586d5db1afb7ba97ea3f03ba06b3c
fxY8e90 <kogure.takashi@fujixerox.co.jp>
2021-03-26 16:26:54 +0900

    [add]テスト追加

--- dockerblog/settings.py
+++ dockerblog/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PASSWORD': 'password',
         'HOST': 'db',
         'PORT': 5432,
+        'TEST': {
+            'NAME': 'test_postgres',
+        }
     }
 }
 
--- trackapp/tests.py
+++ trackapp/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from trackapp.models import Tracks
+
+
+# 必要なテストケース
+"""
+: DBの中身確認
+: URLに紐づくHTMLファイルが正しくレンダリングされているか
+: 入力項目のフォーマットチェック
+: 参考文献　https://nwpct1.hatenablog.com/entry/how-to-write-unittest-on-django
+"""
 
 # Create your tests here.
+class ScraypingTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_db_confirmation(self) -> None:
+        """
+        デフォルトではDBが空の確認
+        :return:
+        """
+        saved_tracks = Tracks.objects.all()
+        self.assertEqual(saved_tracks.count(), 0)
+
+    def test_form_invalid_format(self):
+        """
+         URL入力項目にて、http形式でない入力を行えばエラーになることを検証
+        :return
+        """
+        pass
+    
+    def test_form_correct_format(self):
+        """
+         正常な入力を行えばエラーにならないことを検証
+        :return
+        """
