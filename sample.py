0c1fedf0136315dfdb63a92188e0c54e8ac8ab45
fxY8e90 <kogure.takashi@fujixerox.co.jp>
2021-03-26 16:27:38 +0900

    [add]正規表現・ランキング実装追加

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 import requests
 import bs4
+import re
 
+URL = 'https://www.python.org/'
 # キーワードを使って検索してhtmlを取得する
 keyword = 'python'
 response = requests.get('https://www.google.co.jp/search?q=' + keyword)
@@ -14,20 +16,28 @@ bs = bs4.BeautifulSoup(response.text, "html.parser")
 # 検索結果のタイトルとリンクを取得
 element = bs.select('.kCrYT > a')
 
+rank = 0
+rank_list = []
 title_list = []
 url_list = []
 
 for i in range(len(element)):
     # タイトルのテキスト部分のみ取得
-    title = element[i].get_text()    
+    title = element[i].get_text()
     # リンクのみを取得し、余分な部分を削除する
     url = element[i].get('href').replace('/url?q=','')
+    rank +=1
 
-    title_list.append(title)
-    url_list.append(url)
+    if re.search(URL, url):
+        rank_list.append(rank)
+        title_list.append(title)
+        url_list.append(url)
+    else:
+        continue
 
 # 出力
 for i in range(len(title_list)):
+    print(str(rank_list[i]) + '位')
     print(title_list[i])
     print('['+ url_list[i] +']')
     print('')
